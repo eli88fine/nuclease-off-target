787c7bc58057f3da95a7274d9dc1131d78b278a2
Eli Fine <ubuntu@ip-172-31-89-59.ec2.internal>
2020-10-02 16:41:42 +0000

    removed package version metadata from __init__

--- src/nuclease_off_target/__init__.py
+++ src/nuclease_off_target/__init__.py
@@ -1,11 +1,2 @@
 # -*- coding: utf-8 -*-
 """Docstring."""
-
-try:  # adapted from https://packaging.python.org/guides/single-sourcing-package-version/
-    from importlib import metadata
-except ImportError:  # pragma: no cover
-    # Running on pre-3.8 Python; use importlib-metadata package
-    import importlib_metadata as metadata  # type: ignore # Eli (9/1/20): for some reason mypy is giving weird errors for this
-__version__: str = metadata.version("change_this_to_name_of_package")  # type: ignore # Eli (9/1/20): for some reason mypy is giving weird errors for this
-
-__all__ = ["__version__"]
